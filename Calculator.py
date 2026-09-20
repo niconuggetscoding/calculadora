d4d6d0410fc53d4245fe79886e31788f3c8b1e2e
niconuggetscoding <nickin.concept_computer@yahoo.com>
2026-09-20 13:35:33 -0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+print('Teste de Calculadora')
+print('')
+prinum=float(input('Primeiro número '))
+segnum=float(input('Segundo número '))
+jntr=prinum+segnum
+print('A soma entre {} e {} vale {}'.format(prinum, segnum, jntr))
+
